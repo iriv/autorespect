6fd19418489d1bb4b3771da2edff5da29737274d
Ilya Rysenkov <irisenkov@gmail.com>
2014-07-08 20:15:49 +0400

    test

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
-	phpinfo();
+	require_once 'lib/Twig/Autoloader.php';
+	Twig_Autoloader::register();
+
+	$loader = new Twig_Loader_Filesystem('tpl');
+	$twig = new Twig_Environment($loader, array(
+	    'cache'       => 'cache',
+	    'auto_reload' => true
+	));
+
+	if ($_GET["page"]){
+		$page = $_GET["page"];
+	}
+	else{
+		$page = "main_cr";
+	}
+
+	echo $twig->render($page . '.html',array('tplname' => $page));
 ?>
